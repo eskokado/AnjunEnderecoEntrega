04ad676f12854697e92b96baf7f12cdf5f186346
eskokado <eskokado@gmail.com>
2023-04-05 11:01:25 -0300

    feat: first commit

---
+++ src/main/java/com/anjun/eskokado/anjun_endereco_entrega/AnjunEnderecoEntregaApplication.java
@@ -0,0 +1,13 @@
+package com.anjun.eskokado.anjun_endereco_entrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnjunEnderecoEntregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnjunEnderecoEntregaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anjun/eskokado/anjun_endereco_entrega/AnjunEnderecoEntregaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anjun.eskokado.anjun_endereco_entrega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnjunEnderecoEntregaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
